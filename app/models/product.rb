63b25b965db6be9904c64606b535153f55f62ad5
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-17 15:59:23 -0800

    Add scopes to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ApplicationRecord
     has_many :reviews, dependent: :destroy
+
+    scope :most_recent, -> { order(created_at: :desc).limit(3) }
+    scope :by_country, -> (country_name_parameter) {
+        where('country_of_origin ilike ?', "%#{country_name_parameter}%")
+    }
+    scope :most_tasks, -> {(
+        select('products.id, COUNT(review.id) AS review_count')
+        .joins(:reviews)
+        .group('products.id')
+        .order('review_count DESC')
+        .take(1)
+        .first
+    )}
 end
