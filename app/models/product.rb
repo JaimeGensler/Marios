a897cdd98e016936831e43af6e2af0e31372bed6
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-17 16:06:35 -0800

    Add validations and tests for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
     has_many :reviews, dependent: :destroy
 
+    validates :name, presence: true
+    validates :cost, presence: true
+    validates :country_of_origin, presence: true
+
     scope :most_recent, -> { order(created_at: :desc).limit(3) }
     scope :by_country, -> (country_name_parameter) {
         where('country_of_origin ilike ?', "%#{country_name_parameter}%")
