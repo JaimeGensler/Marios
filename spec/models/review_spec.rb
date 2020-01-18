32eb1a972495999d4a8ac4c33b6afec2ebd5b884
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-17 16:19:19 -0800

    Add validations and tests for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 describe Review do
-    context 'associations' do
+    context '(associations)' do
         it { should belong_to(:product) }
     end
 
-    # context 'validations' do
-    #     it { should validate_presence_of(:author) }
-    #     it { should validate_presence_of(:content_body) }
-    #
-    #     it { should validate_length_of(:author).is_at_most(150) }
-    #     it { should validate_length_of(:content_body).is_at_most(300) }
-    #
-    #     it do
-    #         should validate_numericality_of(:rating).
-    #         is_less_than_or_equal_to(5).
-    #         is_greater_than_or_equal_to(1)
-    #     end
-    # end
+    context '(validations)' do
+        it { should validate_presence_of(:author) }
+        it { should validate_presence_of(:content_body) }
+        it { should validate_presence_of(:rating) }
+
+        it { should validate_length_of(:content_body).is_at_least(50) }
+        it { should validate_length_of(:content_body).is_at_most(250) }
+
+        it { should validate_numericality_of(:rating).only_integer }
+        it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1) }
+        it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+    end
 end
