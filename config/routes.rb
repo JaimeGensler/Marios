e75355ed66f913f183380ebebbf7f866b6580c9b
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-24 10:19:41 -0800

    Add user/session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
     root to: 'homes#index'
+
+    #register
+    get '/register',  to: 'users#new',         as: 'new_user'
+    post '/users',    to: 'users#create'
+
+    #sign in/out
+    get '/signin',    to: 'sessions#new',      as: 'new_session'
+    post '/signin',   to: 'sessions#create'
+    get '/signout',   to: 'sessions#destroy',  as: 'destroy_session'
+
     resources :products do
         resources :reviews, except: [:index]
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User, :model do
     context '(associations)' do
         it { should have_many :reviews }
     end
+    
     context '(validations)' do
         it { should validate_presence_of :username }
         it { should validate_uniqueness_of :username }
