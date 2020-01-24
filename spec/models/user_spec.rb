267bf39f4e750773b8c3f9a4cf8e936ad4c9d303
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-24 09:12:07 -0800

    Add spec coverage for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe User, :model do
+    context '(validations)' do
+        it { should validate_presence_of :username }
+        it { should validate_uniqueness_of :username }
+
+        it { should validate_presence_of :email }
+        it { should validate_uniqueness_of :email }
+
+        it { should validate_confirmation_of :password }
+    end
+
+    context '(encryption)' do
+        it 'encrypts password before save, does not save password' do
+            User.destroy_all
+            User.create!({username: 'Test', email: 'test@test.com', password: 'password'})
+            user = User.authenticate('Test', 'password')
+            expect(user.password_hash).not_to include ('password')
+            expect(user.password).to eq nil
+        end
+    end
+
+    context '(authentication)' do
+        it 'authenticates entered credentials' do
+            User.destroy_all
+            user = User.create!({username: 'Test', email: 'test@test.com', password: 'password'})
+            expect(User.authenticate('Test', '12345')).to eq nil
+            expect(User.authenticate('Test', 'password')).to eq user
+        end
+    end
+end
