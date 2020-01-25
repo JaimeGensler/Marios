e6341a8c2f0eec42880b971e87929c5813dcfc0c
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-24 16:56:07 -0800

    Re-do seeding of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 Product.destroy_all
 Review.destroy_all
+User.destroy_all
+
+#admin is only created here
+User.create!(username: 'admin', email: 'admin', password: 'admin', admin: true)
+
+bojack = Faker::TvShows::BojackHorseman
+40.times do |i|
+    User.create!(
+        username: "#{bojack.character}#{i}",
+        email: "example#{i}@example.com",
+        password: 'password'
+    )
+end
 
 50.times do |i|
     Product.create!(
@@ -9,10 +22,9 @@ Review.destroy_all
     )
 end
 
-bojack = Faker::TvShows::BojackHorseman
 250.times do |i|
-    Product.random.reviews.create!(
-        author: bojack.character,
+    User.random.reviews.create!(
+        product_id: Product.random.id,
         content_body: "#{bojack.quote}. #{bojack.tongue_twister}".truncate(250),
         rating: (1 + rand(5))
     )
@@ -20,3 +32,4 @@ end
 
 p "Created #{Product.count} products."
 p "Created #{Review.count} reviews."
+p "Created #{User.count} users."
