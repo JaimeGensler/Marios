5c7e54401b705aab524ea8fe4d31ca955fadb1ae
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-26 14:49:05 -0800

    Finalize about page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,11 +3,10 @@ class HomesController < ApplicationController
         @recents = Product.most_recent
         @top = Product.most_reviews
         @usa = Product.by_country('United States of America')
-        render :index
     end
 
     def about
-        @blurb = Faker::Lorem.paragraph(sentence_count: 35, supplemental: true)
-        render :about
+        @quote = Faker::TvShows::BojackHorseman.quote
+        @quote += (!/[?.!]\z/.match?(@quote) ? '.' : '')
     end
 end
