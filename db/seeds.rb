02110d78e2aa5655fba480ec872b8593c7d04b29
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-26 16:49:29 -0800

    Re-order seeding so that admin account will not add reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@ Product.destroy_all
 Review.destroy_all
 User.destroy_all
 
-#admin is only created here
-User.create!(username: 'admin', email: 'admin', password: 'admin', admin: true)
-
 bojack = Faker::TvShows::BojackHorseman
 40.times do |i|
     User.create!(
@@ -32,6 +29,9 @@ end
         rating: (1 + rand(5))
     )
 end
+
+#admin creation only occurs here
+User.create!(username: 'admin', email: 'admin', password: 'admin', admin: true)
 
 p "Created #{Product.count} products."
 p "Created #{Review.count} reviews."
